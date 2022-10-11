2010097b9afc5b1444c416cb0eb9bad3a756227f
ajimbar <ajimbar@outlook.com>
2022-10-11 10:15:44 -0600

    Creacion de Jbutton

--- src/fabrica_Concreta/Fabrica_JButton.java
+++ src/fabrica_Concreta/Fabrica_JButton.java
@@ -1,4 +1,22 @@
 package fabrica_Concreta;
 
+import producto_Abstracto.Component;
+import producto_Concreto.Producto_JButton;
+
+import javax.swing.*;
+
 public class Fabrica_JButton {
+
+
+    public Component crearComponente() {
+       Producto_JButton button = new Producto_JButton();
+
+        button.agregarTexto("Botón");
+        button.setIdentificador(button.generarIdentificador());
+        button.agregarEnContenedor(new JFrame());
+        button.esVisible(true);
+        button.agregarDimensiones(500,800,20,20);
+
+        return  button;
+    }
 }
